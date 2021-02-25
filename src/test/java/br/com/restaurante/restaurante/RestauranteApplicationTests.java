ebe2c758177c42a30b7fd099c75bda748e5e260f
brrimolo <bruno.rimolo@gmail.com>
2021-02-24 21:44:32 -0300

    Bruno: first commit

---
+++ src/test/java/br/com/restaurante/restaurante/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
