bc81fabd538b8e512ba4c177c082a4dac7ea02f0
brrimolo <bruno.rimolo@gmail.com>
2021-03-06 20:25:43 -0300

    correcao

--- src/main/java/br/com/restaurante/restaurante/controller/ClienteControl.java
+++ src/main/java/br/com/restaurante/restaurante/controller/ClienteControl.java
@@ -1,7 +1,18 @@
 package br.com.restaurante.restaurante.controller;
 
-import br.com.restaurante.restaurante.repository.ClienteRepo;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.restaurante.restaurante.repository.ClienteRepo;
+import br.com.restaurante.restaurante.model.Cliente;
+
+@RestController
+@RequestMapping("/cliente")
 public class ClienteControl {
     
     private final ClienteRepo clienteRepo;
@@ -10,4 +21,14 @@ public class ClienteControl {
         this.clienteRepo = clienteRepo;
     }
     
+    @GetMapping("/")
+    public List<Cliente> getClientes() {
+        return clienteRepo.findAll();
+    }
+
+    @PostMapping("/")
+    public void gravar(@RequestBody Cliente cliente) {
+        clienteRepo.save(cliente);
+    }
+
 }
